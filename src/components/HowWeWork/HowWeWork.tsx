b6f82fb916ccc7a6b4cd3909ff7413c09b2195d7
cambrown101 <cameron-brown@live.com>
2021-05-17 17:19:11 -0700

    Create HowWeWork component

---
+++ src/components/HowWeWork/HowWeWork.tsx
@@ -0,0 +1,31 @@
+// HOW WE WORK
+
+// WE CREATE A PLATFORM AND STRATEGY SO YOU CAN FOCUS ON THE OPERATIONS OF YOUR BUSINESS
+
+// Full website redesign and development
+
+// eCommerce store development
+
+// Marketing services including content creation and social media strategy
+
+// GROW WITH US – Link to Google Form
+
+import React from "react";
+import styled from "styled-components";
+import Footer from "../Footer/Footer";
+
+const WhyAcclamation = () => {
+  return (
+    <WhyAcclamationWrapper>
+      <Footer />
+    </WhyAcclamationWrapper>
+  );
+};
+
+export default WhyAcclamation;
+
+const WhyAcclamationWrapper = styled.main`
+  h1 {
+    margin: 0;
+  }
+`;
